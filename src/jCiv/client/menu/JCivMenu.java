a8cf71ee802cb4ab87ebd8de5227a3f944620dd2
Tehsmash <sam@code-smash.net>
2012-05-24 15:33:43 +0100

    More work on the client and menus;
    Signed-off-by: Tehsmash <sam@code-smash.net>

--- src/jCiv/client/menu/JCivMenu.java
+++ src/jCiv/client/menu/JCivMenu.java
@@ -1,9 +1,10 @@
 package jCiv.client.menu;
 
+import jCiv.client.Game;
 import jCiv.client.InputHandler;
 import jCiv.client.gui.Screen;
 
 public interface JCivMenu {
-	public void tick(InputHandler ih);
+	public void tick(InputHandler ih, Game game);
 	public void render(Screen screen);
 }
