b5105bb1b5dd32f33643bdd98abbef57f52e46d6
Tehsmash <sam@code-smash.net>
2012-05-24 11:23:38 +0100

    Started adding functionality to client, beginning with drawing text to the screen;
    Signed-off-by: Tehsmash <sam@code-smash.net>

--- src/jCiv/client/Art.java
+++ src/jCiv/client/Art.java
@@ -1,5 +1,33 @@
 package jCiv.client;
 
-public class Art {
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import jCiv.client.gui.Bitmap;
+
+public class Art {
+	
+	public static final String fontLocation = "/art/font.png";
+	
+	public static final Bitmap font = loadArt(fontLocation);
+	
+	public static Bitmap loadArt(String location) {
+		try {
+			BufferedImage img = ImageIO.read(Art.class.getResource(location));
+			
+			int w = img.getWidth();
+			int h = img.getHeight();
+			
+			Bitmap result = new Bitmap(w, h);
+			
+			img.getRGB(0, 0, w, h, result.pixels, 0, w);
+			
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}	
+	}
 }
