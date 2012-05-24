4b5915091ff57880e8271864a9ca8566ff495d28
littlewoo <jdl8@kent.ac.uk>
2012-05-24 17:51:27 +0100

    LoaderTest deals with failure.

--- src/testing/LoaderTest.java
+++ src/testing/LoaderTest.java
@@ -1,16 +1,28 @@
 package testing;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 import jCiv.fileHandling.*;
 import jCiv.map.*;
 
 public class LoaderTest {
 	public static void main(String[] args) {
-		NationLoader nl = new NationLoader("resource/data/nations.xml");
-		List<Nation> ns = nl.getNations();
-		for (Nation n : ns) {
-			System.out.println(n);
+		NationLoader nl;
+		List<Nation> ns;
+		try {
+			nl = new NationLoader("resource/data/nations.xml");
+			ns = nl.getNations();
+			for (Nation n : ns) {
+				System.out.println(n);
+			}
+		} catch (IOException | SAXException | ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
