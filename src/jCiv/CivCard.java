907e6fcf491da578b6813a22e15bd8d71c32af03
Edward Bowles <stealthbow@gmail.com>
2012-05-28 18:15:56 +0100

    Fixed a typo in the xml, completed CivCard.java

--- src/jCiv/CivCard.java
+++ src/jCiv/CivCard.java
@@ -1,5 +1,22 @@
 package jCiv;
 
 public class CivCard {
+    private String[] type;
+    private String text;
+    private int[] discount;
+    private int max, score;
 
+    public CivCard(String[] type, String text, int[] discount, int max, int score) {
+        this.type = type;
+        this.text = text;
+        this.discount = discount;
+        this.max = max;
+        this.score = score;
+    }
+
+    public String[] getType() { return type; }
+    public String getText() { return text; }
+    public int[] getDiscount() { return discount; }
+    public int getMax() { return max; }
+    public int getScore() { return score; }
 }
